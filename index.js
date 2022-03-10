2e8b7aa9099cfd80706dd69d9fa8a34293d583d4
HyeokjinKang <coupy@croissant.tech>
2022-03-10 23:43:55 +0900

    fix:창 크기 조절 및 상단바 메뉴 제거

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const createWindow = () => {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
   window = new BrowserWindow({
-    width: width / 1.25,
-    height: height / 1.25,
+    width: 1000,
+    height: 700,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
+  window.setMenuBarVisibility(false);
   window.loadFile("dist/index.html");
 };
 
