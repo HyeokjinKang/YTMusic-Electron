24b0b3d8778bb9691833ffe2370e711be0fd5ebb
HyeokjinKang <coupy@croissant.tech>
2022-03-14 00:23:26 +0900

    feat: hover된 요소에 대한 설명을 제공하는 UI를 추가합니다.

--- src/components/Home/Default.svelte
+++ src/components/Home/Default.svelte
@@ -3,7 +3,40 @@
 </script>
 
 <div id="curationContainer">
-  <div id="imageContainer" style="background-image: url('{data.thumbnails[data.thumbnails.length - 1].url}')"></div>
+  <div id="imageContainer" style="background-image: url('{data.thumbnails[data.thumbnails.length - 1].url}')">
+    <div id="infoContainer">
+      <span id="infoLeft">
+        {#if data.year}
+          Album
+        {:else if data.artists}
+          {#if data.artists[0].name == "Song" && data.artists.length > 1}
+            Song
+          {:else}
+            Video
+          {/if}
+        {:else if data.subscribers}
+          Artist
+        {:else if data.description}
+          Mix
+        {:else}
+          Playlist
+        {/if}
+      </span>
+      <span id="infoRight">
+        {#if data.year}
+          {data.year}
+        {:else if data.artists}
+          {#if data.artists[0].name == "Song" && data.artists.length > 1}
+            {data.artists[1].name}
+          {:else}
+            {data.artists[0].name}
+          {/if}
+        {:else}
+          <!-- none -->
+        {/if}
+      </span>
+    </div>
+  </div>
   <span id="curationTitle">{data.title}</span>
   {#if data.description}
     <span id="curationExplanation">{data.description}</span>
@@ -24,6 +57,10 @@
   }
 
   #imageContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
     flex: 0 0 auto;
     width: 12vw;
     height: 12vw;
@@ -43,6 +80,38 @@
     font-weight: 700;
     max-height: 3.2em;
     overflow: hidden;
+  }
+
+  #infoContainer {
+    box-sizing: border-box;
+    padding-left: 0.5vw;
+    padding-right: 0.5vw;
+    width: 100%;
+    backdrop-filter: blur(5px);
+    background-color: #000000aa;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    height: 1vw;
+    opacity: 0;
+    border-radius: 0 0 1vh 1vh;
+    transition-duration: .3s;
+  }
+
+  #imageContainer:hover > #infoContainer {
+    opacity: 1;
+    height: 1.5vw;
+  }
+
+  #infoLeft {
+    font-size: 1.7vh;
+    font-weight: 600;
+  }
+
+  #infoRight {
+    font-size: 1.7vh;
+    font-weight: 400;
   }
 
   #curationExplanation {
